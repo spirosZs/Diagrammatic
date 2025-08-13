c0d37b0f8c36eac3f52eb3eb3ded11472ff1c9d2
Spiros Zisis <spiroszs24@gmail.com>
2025-08-13 18:48:37 +0300

    DiagramEvaluation changes(works)

--- Web_API/Exercises/Controllers/DiagramEvaluationController.cs
+++ Web_API/Exercises/Controllers/DiagramEvaluationController.cs
@@ -16,6 +16,72 @@ namespace Exercises.Controllers
 
         private void LoadDiagramAssets()
         {
+            var ports = new List<DiagramPort>
+            {
+                new DiagramPort
+                {
+                    Id = "portTop",
+                    Shape = PortShapes.Circle,
+                    Offset = new DiagramPoint { X = 0.5, Y = 0 },
+                    Visibility = PortVisibility.Visible
+                },
+                new DiagramPort
+                {
+                    Id = "portRight",
+                    Shape = PortShapes.Circle,
+                    Offset = new DiagramPoint { X = 1, Y = 0.5 },
+                    Visibility = PortVisibility.Visible
+                },
+                new DiagramPort
+                {
+                    Id = "portBottom",
+                    Shape = PortShapes.Circle,
+                    Offset = new DiagramPoint { X = 0.5, Y = 1 },
+                    Visibility = PortVisibility.Visible
+                },
+                new DiagramPort
+                {
+                    Id = "portLeft",
+                    Shape = PortShapes.Circle,
+                    Offset = new DiagramPoint { X = 0, Y = 0.5 },
+                    Visibility = PortVisibility.Visible
+                }
+            };
+
+                    var basicNodes = new List<DiagramNode>
+            {
+                new DiagramNode
+                {
+                    Id = "Ellipse",
+                    Shape = new DiagramBasicShape { Type = Shapes.Basic, Shape = BasicShapes.Ellipse },
+                    Ports = ports
+                },
+                new DiagramNode
+                {
+                    Id = "Star",
+                    Shape = new DiagramBasicShape { Type = Shapes.Basic, Shape = BasicShapes.Star },
+                    Ports = ports
+                }
+            };
+
+            ViewBag.BasicShapes = basicNodes;
+
+            var palettes = new List<SymbolPalettePalette>
+    {
+        new SymbolPalettePalette { Id = "basic", Symbols = basicNodes, Title = "Symbols" }
+    };
+            ViewBag.palettes = palettes;
+
+            var margin = new DiagramMargin { Left = 15, Bottom = 15, Right = 15, Top = 15 };
+            ViewBag.margin = margin;
+
+            ViewBag.getNodeDefaults = "getNodeDefaults";
+            ViewBag.getSymbolInfo = "getSymbolInfo";
+            ViewBag.valueChanged = "valueChanged";
+        }
+
+       /* private void LoadDiagramAssets()
+        {
             var basicNodes = new List<DiagramNode>();
             basicNodes.Add(new DiagramNode
                 {Id = "Ellipse", Shape = new DiagramBasicShape {Type = Shapes.Basic, Shape = BasicShapes.Ellipse}});
@@ -34,6 +100,6 @@ namespace Exercises.Controllers
             ViewBag.getNodeDefaults = "getNodeDefaults";
             ViewBag.getSymbolInfo = "getSymbolInfo";
             ViewBag.valueChanged = "valueChanged";
-        }
+        }*/
     }
 }
