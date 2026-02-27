3a8184d4235936ff923dcb5572a00dd21c3a42b7
Spiros Zisis <spiroszs24@gmail.com>
2026-02-27 16:43:58 +0200

    PathDto addition

---
+++ Diagrammatic/Components/Pages/Exercises/Dto/Path/PathExerciseCreateDto.cs
@@ -0,0 +1,22 @@
+﻿using Diagrammatic_test.Components.Pages.Exercises.Dto.Diagram;
+
+namespace Diagrammatic_test.Components.Pages.Exercises.Dto.Path
+{
+    public class PathExerciseCreateDto : ExerciseCreateDto
+    {
+        /// <summary>
+        /// A diagram definition for this Exercise.
+        /// </summary>
+        public DiagramExerciseCreateDto Diagram { get; set; }
+
+        /// <summary>
+        /// The correct paths solution for this Exercise as specified by the teacher.
+        /// </summary>
+        public ICollection<PathDto> Paths { get; set; } = new List<PathDto>();
+
+        /// <summary>
+        /// The code that will be presented to the students when asked to find the correct paths.
+        /// </summary>
+        public string Code { get; set; }
+    }
+}
